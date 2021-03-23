0256c1bc30eedb865f4b62eaa757a21b22536ffd
Laurie Waller <lauriewaller7@gmail.com>
2021-03-22 20:32:57 -0700

    update service files with correct api endpoint

---
+++ src/js/recipe-one-service.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default class RecipeOne {
+  static getRecipeOne(ingredientOne) {
+    return fetch(`https://api.edamam.com/search?q=${ingredientOne}&app_id=${process.env.APP_ID}&app_key=$${process.env.API_KEY}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.status);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return Error(error);
+      }) 
+  }
+}
---
+++ src/js/recipe-two-service.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default class RecipeTwo {
+  static getRecipeTwo(ingredientTwo) {
+    return fetch(`https://api.edamam.com/search?q=${ingredientTwo}&app_id=${process.env.APP_ID}&app_key=$${process.env.API_KEY}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.status);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return Error(error);
+      }) 
+  }
+}
+
+
